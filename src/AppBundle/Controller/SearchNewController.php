29243f11854f6913b18c587830331c78542c5232
Jason Gessner <jason@multiply.org>
2026-02-24 23:26:08 -0600

    Add skeleton of V3 search UI.

---
+++ src/AppBundle/Controller/SearchNewController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SearchNewController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getAction(Request $request)
+    {
+        return $this->render('/search/search_new.html.twig', [
+            'pagetitle'       => "Card Search (new)",
+            'pagedescription' => "New Card Search",
+            'query'           => $request->query->get('q') ?: ''
+        ]);
+    }
+}
